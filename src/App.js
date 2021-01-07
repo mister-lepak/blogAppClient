20017e117225f65e4a92bdf601c49b08d2589ac9
Try Wijono <3wijono@gmail.com>
2021-01-07 21:20:08 +0800

    initiation, index data fetching and header bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const fetchIndexData = async () => {
+    try {
+      const indexResponse = await fetch("http://localhost:5000/", {
+        mode: "cors",
+      });
+      const index = await indexResponse.json();
+      console.log(index);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchIndexData();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui dividing clearing segment">
+      <h1 className="ui left floated header">Blog App</h1>
+      <div className="ui right floated header">
+        <button className="ui button">
+          <h3>Log In</h3>
+        </button>
+      </div>
+      <div className="ui right floated header">
+        <button className="ui button">
+          <h3>Sign Up</h3>
+        </button>
+      </div>
     </div>
   );
 }
